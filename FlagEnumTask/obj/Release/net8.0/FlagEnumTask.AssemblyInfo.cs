7ac87d32dfa8a350286cb60b8331a4616ec7e255
Youssef Waeel <youssef.waeel.haroon@gmail.com>
2025-08-06 15:25:22 +0300

    Some images for output

--- FlagEnumTask/obj/Release/net8.0/FlagEnumTask.AssemblyInfo.cs
+++ FlagEnumTask/obj/Release/net8.0/FlagEnumTask.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlagEnumTask")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0e5b3777ffccdae33a5dfb2a369e1a413f14507")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlagEnumTask")]
